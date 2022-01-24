89811e6468c562ce34efa7abe1cdaf5fae966377
Denis <den.kuchma10@gmail.com>
2022-01-24 15:30:31 +0200

    mMoveBall

---
+++ moveBall.py
@@ -0,0 +1,32 @@
+WIDTH = 30
+HEIGHT = 10
+ballX = 5
+ballY = 1
+rocket1 = 0
+rocket2 = 6
+rocketLength = 2
+dirX = 1
+dirY = 1
+
+
+def moveBall():
+    global ballX, ballY, dirX, dirY
+    if ballY > HEIGHT - 2 or ballY < 1:
+        dirY *= -1 
+    if ballX > WIDTH - 3\
+            and ballY >= rocket2 \
+            and ballY < rocket2 + rocketLength:
+        dirX *= -1 
+    elif ballX == WIDTH - 1:
+        exit("Player 1 win")
+    if ballX < 2 \
+            and ballY >= rocket1 \
+            and ballY < rocket1 + rocketLength:
+        dirX *= -1
+    if ballX < 0:
+        dirX *= -1
+    elif ballX == 0:
+        exit("Player 2 win")
+    
+    ballX += dirX
+    ballY += dirY
