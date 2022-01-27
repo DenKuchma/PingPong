73d2a9e7a4b163c058e888fd687f7f733af22dfd
Denis <den.kuchma10@gmail.com>
2022-01-27 15:43:20 +0200

    work game

--- moveBall.py
+++ moveBall.py
@@ -2,7 +2,6 @@ import time
 import os
 import pynput
 
-
 WIDTH = 30
 HEIGHT = 10
 ballX = 5
@@ -12,7 +11,6 @@ rocket2 = 6
 rocketLength = 2
 dirX = 1
 dirY = 1
-
 
 def moveBall():
     global ballX, ballY, dirX, dirY
@@ -36,7 +34,49 @@ def moveBall():
     ballX += dirX
     ballY += dirY
 
+def draw():
+    y = 0
+    while y < HEIGHT:
+        x = 0
+        result = ''
+        while x < WIDTH:
+            if x == ballX and y == ballY:
+                result += 'o'
+            elif x == 0 \
+                    and y >= rocket1 \
+                    and y < rocket1 + rocketLength:
+                result += '|'
+            elif x == WIDTH - 1 \
+                    and y >= rocket2 \
+                    and y < rocket2 + rocketLength:
+                result += '|'
+            else:
+                result += '-'
+            x += 1
+        print(result)
+        y += 1
+
+def on_press(key):
+    global rocket2, rocket1
+    if key == pynput.keyboard.Key.up and rocket2 > 0 and rocket2 <= 8:
+        rocket2 -= 1
+    if key == pynput.keyboard.Key.down and rocket2 >= 0 and rocket2 < 8:
+        rocket2 += 1
+    if key == pynput.keyboard.KeyCode.from_char('w') and rocket1 > 0 and rocket1 <= 8:
+        rocket1 -= 1
+    if key == pynput.keyboard.KeyCode.from_char('s') and rocket1 >= 0 and rocket1 < 8:
+        rocket1 += 1
+    
+def on_release(key):
+    pass
+    
+pynput.keyboard.Listener(
+    on_press = on_press,
+    on_release = on_release
+).start()
+
 while True:
     os.system('cls')
+    draw()
     moveBall()
     time.sleep(0.1/2)
